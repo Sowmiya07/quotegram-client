fe9c9da05bcee6f4ddc0cd24778e5985260178e5
Ravikumar <sowmiya.a.ravikumar@accenture.com>
2020-04-11 22:17:55 +0530

    Commit #1
    1. Spinned the project with the CRA and removed unnecessary files
    2. Added the html2canvas library to the project to capture the
       screenshot of the part of the webpage and convert it to canvas
    3. created a image element from canvas and opened it in new tab of the
       browser window

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import html2canvas from 'html2canvas';
 
 function App() {
+
+  function createQuote() {
+    console.log("Create Quote Button is Clicked")
+  }
+
+  function saveQuote() {
+    console.log("Save Quote Button is Clicked")
+    html2canvas(document.getElementById("capture"))
+      .then(canvas => {
+        let imageUrl = canvas.toDataURL('image/png');
+        let image = new Image();
+        image.src=imageUrl;
+        let newTab = window.open()
+        newTab.document.body.appendChild(image)
+      }
+      );
+  }
+
+
+  function openLink() {
+    document.getElementById("download").click();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <button onClick={createQuote} >
+        Create New Quote  
+      </button>
+      <div id="capture" style={{padding: "50px"}}>
+        <h4>Hello World!!!</h4>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <button onClick={saveQuote}>
+        Save The Quote
+      </button>
     </div>
   );
 }
