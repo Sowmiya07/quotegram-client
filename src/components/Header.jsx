aa1ca56b297da326c926846311aeef30598b95b3
Ravikumar <sowmiya.a.ravikumar@accenture.com>
2020-05-03 00:24:40 +0530

    Added the ModalWindow component to upload images and send it to server

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import '../styles/Header-Styles.css';
+import ModalWindow from './ModalWindow';
 
-const createNewPost = (event) => {
-    console.log(event.target);
-}
+class Header extends React.Component {
 
-function Header() {
-    return (
-        <div className="header">
-            <h1>Quotegram</h1>
-            <ul className="links">
-                <li>HOME</li>
-                <li className="new-post" onClick={createNewPost}>NEW POST</li>
-            </ul>
-        </div>
-    )
+        constructor() {
+            super();
+            this.state = {
+                showModal: false
+            }
+        }
+
+        createNewPost = () => {
+            this.setState((prevState) => ({showModal: !prevState.showModal}))
+        }
+
+        render() {
+            return (
+            <div className="header">
+                <h1>Quotegram</h1>
+                <ul className="links">
+                    <li>HOME</li>
+                    <li className="new-post" onClick={this.createNewPost}>NEW POST</li>
+                </ul>
+                {this.state.showModal ? <ModalWindow /> : null}
+            </div>
+        )
+    }
 }
 
 export default Header;
---
+++ src/components/ModalWindow.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../styles/ModalWindow.css';
+
+const sendPostToServer = (event) => {
+    const imageFile = document.getElementById("file").files[0];
+    const formData = new FormData();
+    formData.append("file", imageFile);
+    fetch('http://localhost:8080/postImage/', {
+        method: 'POST',
+        body: formData
+    }).then(console.log("success"));
+}
+
+function ModalWindow() {
+    return (
+        <div className="modal">
+            <div className="model-content">
+                <h2>Create New post</h2>
+                <input type="file" className="upload-file" id="file" />   
+                <button className="create-btn" onClick={sendPostToServer}>CREATE POST</button>
+            </div>
+        </div>
+    )
+}
+
+export default ModalWindow;
